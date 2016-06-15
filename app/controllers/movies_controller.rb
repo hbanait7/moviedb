415ae109d0cdb196d4a54adb8a1a24c32326e5c4
hbanait <hbanait7@gmail.com>
2016-06-14 22:24:01 -0400

    Add themoviedb gem. Output some data to index.html to verify everything working

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,6 +2,8 @@ class MoviesController < ApplicationController
   
   def index
     @movies = Tmdb::Movie.now_playing
+    @configuration = Tmdb::Configuration.new
+    @configuration.base_url
   end
 
 end
--- config/initializers/themoviedb.rb
+++ config/initializers/themoviedb.rb
@@ -1,1 +1,9 @@
 Tmdb::Api.key("3f47252d4023426c9b135f168c6413b9")
+
+configuration = Tmdb::Configuration.new
+configuration.base_url
+configuration.secure_base_url
+configuration.poster_sizes
+configuration.backdrop_sizes
+configuration.profile_sizes
+configuration.logo_sizes
